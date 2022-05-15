ca6c5823d01231d17f58295cac61403024a06447
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-05-15 11:43:39 +0300

    CRATE Beams

---
+++ src/Components/Beams/Beams.js
@@ -0,0 +1,49 @@
+
+const Beams = () => {
+
+    return (
+        <main >
+
+            <table >
+                <thead>
+                    <tr>
+                        <th>Height</th>
+                        <th>Width</th>
+                        <th>Concrete</th>
+                        <th>Steel</th>
+                        <th>Rebars</th>
+                    </tr>
+
+                </thead>
+
+                <tbody>
+
+                    <tr>
+                        <td>
+                            <p>600</p>
+                        </td>
+                        <td>
+                            <p>300</p>
+                        </td>
+                        <td>
+                            <p>C25/30</p>
+                        </td>
+                        <td>
+                            <p>B500B</p>
+                        </td>
+                        <td>
+                            <p>12</p>
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
+
+            <h1 >No RC Beams designed so far...</h1>
+
+        </main>
+    );
+
+}
+
+export default Beams;
