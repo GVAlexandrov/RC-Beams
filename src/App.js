6e053bf770ecd81c81bd59421358be6acb8b6c60
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-05-24 16:15:40 +0300

    UPDATE App

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Register from './Components/Register/Register';
 
 import MyBeams from './Components/Beams/Beams';
 import NewBeam from './Components/NewBeam/NewBeam';
-import Slabs from './Components/Slabs/Slabs';
 import Frames from './Components/Frames/Frames';
 
 import Footer from './Components/Footer/Footer';
@@ -29,7 +28,6 @@ function App() {
 
         <Route path='/beams' element={<MyBeams />} />
         <Route path='/beams/new-beam' element={<NewBeam />} />
-        <Route path='/slabs' element={<Slabs />} />
         <Route path='/frames' element={<Frames />} />
       </Routes>
 
