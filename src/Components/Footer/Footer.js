bdad6d955ee8f9875646eecb731f5f767b1f7bf8
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-05-24 16:00:53 +0300

    UPDATE Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,4 +1,6 @@
 // import { NavLink, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
 
 const onBackToTopClick = (e) => {
     window.scrollTo(0, 0);
@@ -7,13 +9,27 @@ const onBackToTopClick = (e) => {
 const Footer = () => {
 
     return (
-        <footer className="one">
-            <a href="#" onClick={onBackToTopClick}> &uarr; Back to top &uarr;</a>
+        <FooterStyled>
+            <AStyled href="#" onClick={onBackToTopClick}> &uarr; Back to top &uarr;</AStyled>
             {/* <img src="https://cdn.dribbble.com/users/2007778/screenshots/15622394/media/4aa0c47e4f65f8709d8817e0a85554d1.jpg?compress=1&resize=400x300" alt="" /> */}
             <p>GVA RC Design 2022</p>
-            <p>@All Rights Reserved &copy;</p>
-        </footer>
+            <p>All Rights Reserved &copy;</p>
+        </FooterStyled>
     )
 }
+
+const FooterStyled = styled.footer`
+background:black;
+color:white;
+height:100px;
+
+`;
+
+const AStyled = styled.a`
+vertical-align:middle;
+color:white;
+text-decoration:none;
+
+`;
 
 export default Footer;
