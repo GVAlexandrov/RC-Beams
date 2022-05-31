60443a97dda607166a75d18d114b1d2dc882d965
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-05-31 20:23:18 +0300

    CREATE authService

---
+++ src/services/authService.js
@@ -0,0 +1,20 @@
+import { firebaseLogin, firebaseRegister, firebaseSignOut } from '../config/firebaseInit';
+
+export function register(email, pass) {
+    return firebaseRegister(email, pass)
+        .then(() => login(email, pass))
+        .catch((error) => console.log(error))
+}
+
+export function login(email, pass) {
+    return firebaseLogin(email, pass);
+}
+
+export function logout() {
+    return firebaseSignOut;
+}
+
+export function activeUser(uid, email) {
+    localStorage.setItem('uid', uid);
+    localStorage.setItem('email', email);
+}
