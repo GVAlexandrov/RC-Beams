d1948dd18b0f646537f38555907b176c9e0c089f
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-06-01 22:23:25 +0300

    CREAT firebaseConfig

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBJKw35dgcXjJjmnWId3SXT9HiwtPDeVMA",
+    authDomain: "rc-beams.firebaseapp.com",
+    projectId: "rc-beams",
+    storageBucket: "rc-beams.appspot.com",
+    messagingSenderId: "533206252555",
+    appId: "1:533206252555:web:20be567ae7413804215b0e"
+};
