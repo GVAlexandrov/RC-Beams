3fb54c3f3f09fe3211ff89c0f96b8553fb3fe9d7
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-06-04 20:30:03 +0300

    UPDATE Login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,21 +1,68 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { login, activeUser } from '../../services/authService'
+import validate from "../../validations/authValidations";
+
+
 
 const Login = () => {
+    const [emailError, setEmailError] = useState('');
+    const [passError, setPassError] = useState('');
+    const [credentials, setCredentials] = useState({
+        email: '',
+        pass: ''
+    });
+
+    const navigate = useNavigate();
+
+
+
+    const onLogin = (e) => {
+        e.preventDefault();
+
+        const emailTextError = validate.username(credentials.email);
+        const passTextError = validate.password(credentials.pass);
+
+        if (emailTextError) {
+            setEmailError(emailTextError);
+            setTimeout(() => {
+                setEmailError('');
+            }, 3000);
+        }
+
+        if (passTextError) {
+            setPassError(passTextError);
+            setTimeout(() => {
+                setPassError('');
+            }, 3000);
+        }
+
+        if (emailError || passError) return;
+
+        login(credentials.email, credentials.pass)
+            .then(res => activeUser(res.user.uid, res.user.email))
+            .then(() => navigate('/beams'))
+            .catch(error => console.log(error));
+    }
+
+
 
     return (
         <main>
-            <form>
+            <form onSubmit={onLogin}>
                 <h1>Login</h1>
 
                 <p>Personal info</p>
 
                 <div>
-                    <label htmlFor="username">Username</label>
-                    <input id="username" type="text" placeholder="JustMyself123..." />
+                    <label htmlFor="email">Email</label>
+                    <input id="email" name="email" type="text" placeholder="your-email@gmail.com" onInput={(e) => setCredentials(oldState => ({ ...oldState, email: e.target.value }))} />
                 </div>
 
                 <div>
-                    <label htmlFor="username">Password</label>
-                    <input id="password" type="password" placeholder="********" />
+                    <label htmlFor="password">Password</label>
+                    <input id="password" name="password" type="password" placeholder="********" onInput={(e) => setCredentials(oldState => ({ ...oldState, pass: e.target.value }))} />
                 </div>
 
                 <button type="submit">Login</button>
