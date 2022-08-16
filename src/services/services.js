6e0feb2830c13b5c3c83678550c57e100627b787
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-08-16 20:18:01 +0300

    UPDATE Services

--- src/services/services.js
+++ src/services/services.js
@@ -1,36 +1,63 @@
 import { URL } from '../config/config';
 import { auth } from '../config/firebaseInit';
 
-export const addNewBeam = (height, width, concrete, steel, rebar) => {
-    const uid = localStorage.uid;
-    console.log(uid);
+const uid = localStorage.uid;
 
-    let newExpense = {
-        height,
-        width,
-        concrete,
-        steel,
-        rebar
-    }
+export const addNewBeam = (height, width, concrete, steel, rebar) => {
+    let newBeam = { height, width, concrete, steel, rebar }
 
     return auth.currentUser.getIdToken(false)
         .then((token) => {
             return fetch(`${URL}beams/${uid}/.json?auth=${token}`, {
                 method: "POST",
-                body: JSON.stringify(newExpense)
+                body: JSON.stringify(newBeam)
             })
         })
-
 }
 
 export const getAllBeams = () => {
-    const uid = localStorage.uid;
-    // console.log(URL + `beams/${uid}.json`);
-
     return fetch(`${URL}beams/${uid}/.json`)
         .then(response => response.json())
         .then(result => {
-            console.log(result);
+            return result;
         })
         .catch(error => console.log(error));
+}
+
+export const getOneBeam = (beamId) => {
+    return fetch(`${URL}beams/${uid}/${beamId}/.json`)
+        .then(response => response.json())
+        .then(result => {
+            return result;
+        })
+        .catch(error => console.log(error));
+}
+
+export const deleteOneBeam = (event, elementId) => {
+    // let parentNode = event.target.nodeName === 'TD' ? event.target.parentNode.parentNode : event.target.parentNode.parentNode.parentNode;
+
+    return auth.currentUser.getIdToken(false)
+        .then((token) => {
+            return fetch(`${URL}beams/${uid}/${elementId}/.json?auth=${token}`, {
+                method: "DELETE",
+            })
+        })
+    // .then(() => {
+    //     getAllBeams()
+    // })
+    // .then(res => {
+    //     parentNode.setBeams(res);
+    // })    
+}
+
+export const editBeam = (height, width, concrete, steel, rebar, beamId) => {
+    let newBeam = { height, width, concrete, steel, rebar }
+
+    return auth.currentUser.getIdToken(false)
+        .then((token) => {
+            return fetch(`${URL}beams/${uid}/${beamId}/.json?auth=${token}`, {
+                method: "PUT",
+                body: JSON.stringify(newBeam)
+            })
+        })
 }
