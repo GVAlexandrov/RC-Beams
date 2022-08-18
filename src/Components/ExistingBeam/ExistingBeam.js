d78f25dc5fb4b0b42fa2038bae1dac36645f47e1
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-08-18 19:58:03 +0300

    UPDATE ExistingBeam

--- src/Components/ExistingBeam/ExistingBeam.js
+++ src/Components/ExistingBeam/ExistingBeam.js
@@ -2,32 +2,29 @@ import styled from 'styled-components';
 
 
 const ExistingBeam = (beamObj) => {
+    const { height, width, concrete, steel, rebar } = beamObj.beam[1];
+    const beamArr = [height, width, concrete, steel, rebar];
+
     return (
         <TrStyled key={beamObj.beam[0]} id={beamObj.beam[0]}>
+            {
+                beamArr.map((x) => {
+                    return (
+                        <td>
+                            <p>{x}</p>
+                        </td>
+                    )
+                })
+            }
+
             <td>
-                <p>{beamObj.beam[1].height}</p>
-            </td>
-            <td>
-                <p>{beamObj.beam[1].width}</p>
-            </td>
-            <td>
-                <p>{beamObj.beam[1].concrete}</p>
-            </td>
-            <td>
-                <p>{beamObj.beam[1].steel}</p>
-            </td>
-            <td>
-                <p>{beamObj.beam[1].rebar}</p>
-            </td>
-            <td>
-                <p>Edit</p>
+                <button>Edit</button>
             </td>
             <td >
-                <p>X</p>
+                <button>X</button>
             </td>
         </TrStyled>
     )
-
 }
 
 const TrStyled = styled.tr`
