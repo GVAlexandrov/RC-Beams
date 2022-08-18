11ac551efaf6a8e454a1112a7a65757c153591df
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-08-18 19:47:21 +0300

    CREATE structuralData

---
+++ src/services/structuralData.js
@@ -0,0 +1,18 @@
+const concreteArr = ['C20/25', 'C25/30', 'C30/37', 'C35/45', 'C50/60'];
+const steelArr = [
+    'B420',
+    'B420A',
+    'B420B',
+    'B420C',
+    'B500',
+    'B500A',
+    'B500B',
+    'B500C'
+];
+const rebarArr = [6.5, 8, 10, 12, 14, 16, 18, 20, 22, 25, 28, 32];
+
+export {
+    concreteArr,
+    steelArr,
+    rebarArr,
+}
