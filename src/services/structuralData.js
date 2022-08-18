923bb1c22c39e4742a5d5c74b9ea568c9069022e
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-08-18 20:11:57 +0300

    UPDATE structuralData

--- src/services/structuralData.js
+++ src/services/structuralData.js
@@ -10,9 +10,29 @@ const steelArr = [
     'B500C'
 ];
 const rebarArr = [6.5, 8, 10, 12, 14, 16, 18, 20, 22, 25, 28, 32];
+const tableHeadingsArr = [
+    'Height',
+    'Width',
+    'Concrete',
+    'Steel',
+    'Rebar Ø',
+    'Edit',
+    'Delete'
+];
+const tableHeadingsDimensionsArr = [
+    '[mm]',
+    '[mm]',
+    '[MPa]',
+    '[MPa]',
+    '[mm]',
+    '[-]',
+    '[-]'
+];
 
 export {
     concreteArr,
     steelArr,
     rebarArr,
+    tableHeadingsArr,
+    tableHeadingsDimensionsArr
 }
