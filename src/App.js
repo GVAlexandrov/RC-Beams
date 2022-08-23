92a4665a04026863887c80ff4def73ae7316f109
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-08-23 23:18:42 +0300

    UPDATE App

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
-import MyBeams from './Components/Beams/Beams';
+import Beams from './Components/Beams/Beams';
 import NewBeam from './Components/NewBeam/NewBeam';
 import EditBeam from './Components/EditBeam/EditBeam';
 import Frames from './Components/Frames/Frames';
@@ -27,16 +27,19 @@ function App() {
     <WrapperDivStyled>
       <Header userEmail={userEmail} setUserEmail={setUserEmail} />
 
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
+      <MainStyled>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
 
-        <Route path='/beams' element={<MyBeams />} />
-        <Route path='/beams/new-beam' element={<NewBeam />} />
-        <Route path='/beams/edit-beam/:beamId' element={<EditBeam />} />
-        <Route path='/frames' element={<Frames />} />
-      </Routes>
+          <Route path='/beams' element={<Beams />} />
+          <Route path='/beams/new-beam' element={<NewBeam />} />
+          <Route path='/beams/edit-beam/:beamId' element={<EditBeam />} />
+
+          <Route path='/frames' element={<Frames />} />
+        </Routes>
+      </MainStyled>
 
 
       <Footer />
@@ -45,11 +48,24 @@ function App() {
 }
 
 const WrapperDivStyled = styled.div`
-    background-color: gray;
-    border: 2px solid black;
-    margin: auto ;
-    text-align: center;
-    height:99vh;
+position:absolute;
+top:0px;
+left:0px;
+width:100%;
+/* min-height:90vh; */
+padding-top:80px;
+background-color: gray;
+text-align: center;
+display:flex;
+    flex-direction: column;    
+  
+`;
+
+const MainStyled = styled.main`
+position:relative;
+top:20px;
+width:100%;
+min-height:65vh;
 `;
 
 export default App;
