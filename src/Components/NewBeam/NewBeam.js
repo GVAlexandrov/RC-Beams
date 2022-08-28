59aa9a9c2e746cc3b3cb97d7a376e41e995f1862
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-08-28 21:06:21 +0300

    UPDATE NewBeam

--- src/Components/NewBeam/NewBeam.js
+++ src/Components/NewBeam/NewBeam.js
@@ -117,19 +117,22 @@ const NewBeam = () => {
                 }
 
                 <DivStyled>
-                    <LabelStyledName htmlFor="bendingMoment">BM</LabelStyledName>
+                    <LabelStyledName htmlFor="bendingMoment">Bending Moment</LabelStyledName>
                     <input id="bendingMoment" name='bendingMoment' type="number" placeholder="10" />
                     <LabelStyledDimension htmlFor="bendingMoment">[kN.m]</LabelStyledDimension>
                 </DivStyled>
 
-                {/* {widthError
-                    ? (
-                        <DivErrStyled >
-                            <p>{widthError}</p>
-                        </DivErrStyled>
-                    )
-                    : (<></>)
-                } */}
+                <DivStyled>
+                    <LabelStyledName htmlFor="shearForce">Shear Force</LabelStyledName>
+                    <input id="shearForce" name='shearForce' type="number" placeholder="10" />
+                    <LabelStyledDimension htmlFor="shearForce">[kN]</LabelStyledDimension>
+                </DivStyled>
+
+                <DivStyled>
+                    <LabelStyledName htmlFor="torsion">Torsion</LabelStyledName>
+                    <input id="torsion" name='torsion' type="number" placeholder="10" />
+                    <LabelStyledDimension htmlFor="torsion">[kN.m]</LabelStyledDimension>
+                </DivStyled>
 
                 <DivStyled>
                     <LabelStyledName htmlFor="concrete">Concrete</LabelStyledName>
