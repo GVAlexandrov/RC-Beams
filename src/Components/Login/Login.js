75dad42edcbe7887c5f9b6be576daea0ac3c20cf
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-08-28 20:43:57 +0300

    UPDATE Login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import { login, activeUser } from '../../services/authService'
 import validate from "../../validations/authValidations";
-
+import styled from 'styled-components';
 
 
 const Login = () => {
@@ -52,7 +52,7 @@ const Login = () => {
 
 
     return (
-        <main>
+        <MainStyled>
             <form onSubmit={onLogin}>
                 <h1>Login</h1>
 
@@ -68,11 +68,33 @@ const Login = () => {
                     <input id="password" name="password" type="password" placeholder="********" onInput={(e) => setCredentials(oldState => ({ ...oldState, pass: e.target.value }))} />
                 </div>
 
-                <button type="submit">Login</button>
+                <ButtonStyled type="submit">Login</ButtonStyled>
             </form>
-        </main>
+        </MainStyled>
     );
 
 }
+
+const MainStyled = styled.main`
+position:relative;
+margin:auto;
+padding-bottom:20px;
+width:60%;
+max-width:500px;
+min-width:300px;
+border: 2px solid black;
+border-top-left-radius:30px;
+border-bottom-right-radius:30px;
+`;
+
+const ButtonStyled = styled.button`
+font-weight:bold;
+color:white;
+background:black;
+padding: 10px 10px;
+border-radius:5px;
+border-color:red;
+cursor: pointer;
+`;
 
 export default Login;
