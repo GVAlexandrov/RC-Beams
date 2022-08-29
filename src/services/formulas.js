8c79e2e002be7d9e01aab1f9ec46613447b0bc73
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-08-29 20:38:30 +0300

    UPDATE formulas

--- src/services/formulas.js
+++ src/services/formulas.js
@@ -10,8 +10,16 @@ function fcmCalculate(fck) {
     return (fck + 8);
 }
 
+function fctmCalculate(fck, fcm) {
+    return (fck <= 50
+        ? 0.3 * fck ** (2 / 3)
+        : 2.12 * Math.log(1 + (fcm / 10))
+    );
+}
+
 export {
     fcdCalculate,
     fydCalculate,
-    fcmCalculate
+    fcmCalculate,
+    fctmCalculate
 }
