3cd6efa105bf978f32e54ef8ddb0ae9fb7ad9a0f
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-09-07 21:14:38 +0300

    UPDATE BeamsGeometry

--- src/Components/NewBeam/BeamsGeometry.js
+++ src/Components/NewBeam/BeamsGeometry.js
@@ -1,14 +1,22 @@
-import { DivStyled, LabelStyledName, LabelStyledDimension, DivErrStyled } from './newBeamStyled'
+import { DivStyled, LabelStyledName, LabelStyledDimension, DivErrStyled, SectionDivStyled, H2Styled } from './newBeamStyled'
 
 const BeamsGeometry = (props) => {
     const heightError = props.heightError;
     const widthError = props.widthError;
 
     return (
-        <>
+        <SectionDivStyled>
+            <H2Styled>Beams geometry</H2Styled>
+
             <DivStyled >
                 <LabelStyledName htmlFor="height">Height</LabelStyledName>
-                <input id="height" name='height' type="number" placeholder="500" />
+                <input
+                    id="height"
+                    name='height'
+                    type="number"
+                    placeholder="500"
+                    defaultValue={props.height ? props.height : ''}
+                />
                 <LabelStyledDimension htmlFor="height">[mm]</LabelStyledDimension>
             </DivStyled>
 
@@ -23,7 +31,13 @@ const BeamsGeometry = (props) => {
 
             <DivStyled>
                 <LabelStyledName htmlFor="width">Width</LabelStyledName>
-                <input id="width" name='width' type="number" placeholder="250" />
+                <input
+                    id="width"
+                    name='width'
+                    type="number"
+                    placeholder="250"
+                    defaultValue={props.width ? props.width : ''}
+                />
                 <LabelStyledDimension htmlFor="width">[mm]</LabelStyledDimension>
             </DivStyled>
 
@@ -35,7 +49,7 @@ const BeamsGeometry = (props) => {
                 )
                 : (<></>)
             }
-        </>
+        </SectionDivStyled>
     )
 }
 
