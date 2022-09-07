ef5652fa0bfaffffd21b5805b350b81b7e257f9a
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-09-07 21:14:49 +0300

    UPDATE BeamsInfo

--- src/Components/NewBeam/BeamsInfo.js
+++ src/Components/NewBeam/BeamsInfo.js
@@ -1,21 +1,23 @@
-import { DivStyled, LabelStyledName, LabelStyledDimension } from './newBeamStyled'
+import { DivStyled, LabelStyledName, LabelStyledDimension, SectionDivStyled, H2Styled } from './newBeamStyled'
 
 const BeamsInfo = (props) => {
 
     return (
-        <>
+        <SectionDivStyled>
+            <H2Styled>Beams general info</H2Styled>
+
             <DivStyled >
                 <LabelStyledName htmlFor="level">Level</LabelStyledName>
-                <input id="level" name='level' type="text" placeholder="First floor / +3.10" />
+                <input id="level" name='level' type="text" placeholder="First floor / +3.10" defaultValue={props.level ? props.level : ''} />
                 <LabelStyledDimension htmlFor="level">[-]</LabelStyledDimension>
             </DivStyled>
 
             <DivStyled >
                 <LabelStyledName htmlFor="beamsNumber">Beam's number</LabelStyledName>
-                <input id="beamsNumber" name='beamsNumber' type="text" placeholder="B01" />
+                <input id="beamsNumber" name='beamsNumber' type="text" placeholder="B01" defaultValue={props.beamsNumber ? props.beamsNumber : ''} />
                 <LabelStyledDimension htmlFor="beamsNumber">[-]</LabelStyledDimension>
             </DivStyled>
-        </>
+        </SectionDivStyled>
     )
 }
 
