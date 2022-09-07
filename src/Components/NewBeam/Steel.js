400d5f7811b841a04ea7f2b0c03a23f06c8d999d
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-09-07 21:19:51 +0300

    UPDATE Steel

--- src/Components/NewBeam/Steel.js
+++ src/Components/NewBeam/Steel.js
@@ -1,25 +1,43 @@
-import { DivStyled, LabelStyledName, LabelStyledDimension, DivErrStyled, InputReadOnlyStyled } from './newBeamStyled'
+import { useState } from 'react';
+
+import { DivStyled, LabelStyledName, LabelStyledDimension, DivErrStyled, InputReadOnlyStyled, H2Styled, SectionDivStyled } from './newBeamStyled';
+
+import { fydCalculate } from '../../services/formulas';
+
+import * as structuralData from '../../services/structuralData';
+
+
 
 const Steel = (props) => {
-    const structuralData = props.structuralData;
-    const setFy = props.setFy;
-    const fy = props.fy;
-    const fyd = props.fyd;
-    const setGammaMS = props.setGammaMS;
+    let [fy, setFy] = useState(0);
+    let [gammaMS, setGammaMS] = useState(0);
+
+    let fyd = 0;
+
+    fyd = fydCalculate(fy, gammaMS);
+
     const steelError = props.steelError;
 
     return (
-        <>
+        <SectionDivStyled>
+            <H2Styled>Steel properties</H2Styled>
+
+
             <DivStyled>
                 <LabelStyledName htmlFor="steel">Steel</LabelStyledName>
                 <select name="steel" id="steel" onChange={e => setFy(Number(e.target.value.slice(1, 4)))}>
+
                     <option disabled selected hidden value="default">Select steel...</option>
+
                     {structuralData.steelArr.map((steelGrade) => {
-                        return <option value={steelGrade}>{steelGrade}</option>
+                        return props.steel === steelGrade
+                            ? < option selected value={steelGrade} > {steelGrade}</option>
+                            : < option value={steelGrade} > {steelGrade}</option>
                     })}
                 </select>
                 <LabelStyledDimension htmlFor="steel">[MPa]</LabelStyledDimension>
             </DivStyled>
+
 
             {steelError
                 ? (
@@ -30,11 +48,13 @@ const Steel = (props) => {
                 : (<></>)
             }
 
+
             <DivStyled>
-                <LabelStyledName htmlFor="fy">fy</LabelStyledName>
-                <InputReadOnlyStyled id="fy" name='fy' type="text" value={fy} />
+                <LabelStyledName readOnly htmlFor="fy">fy</LabelStyledName>
+                <InputReadOnlyStyled readOnly id="fy" name='fy' type="text" value={fy} />
                 <LabelStyledDimension htmlFor="fy">[MPa]</LabelStyledDimension>
             </DivStyled>
+
 
             <DivStyled>
                 <LabelStyledName htmlFor="gammaMS">γm,s</LabelStyledName>
@@ -47,12 +67,13 @@ const Steel = (props) => {
                 <LabelStyledDimension htmlFor="concrete">[MPa]</LabelStyledDimension>
             </DivStyled>
 
+
             <DivStyled>
                 <LabelStyledName htmlFor="fyd">fyd</LabelStyledName>
-                <InputReadOnlyStyled id="fyd" name='fyd' type="text" value={(fyd && fyd !== Infinity && !isNaN(fyd)) ? fyd.toFixed(0) : ''} />
+                <InputReadOnlyStyled readOnly id="fyd" name='fyd' type="text" value={(fyd && fyd !== Infinity && !isNaN(fyd)) ? fyd.toFixed(0) : ''} />
                 <LabelStyledDimension htmlFor="fyd">[MPa]</LabelStyledDimension>
             </DivStyled>
-        </>
+        </SectionDivStyled >
     )
 }
 
