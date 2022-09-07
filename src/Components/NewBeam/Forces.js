d577f56f4ff4bb04bb6298a0b2272647a4490258
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-09-07 21:19:20 +0300

    UPDATE Forces

--- src/Components/NewBeam/Forces.js
+++ src/Components/NewBeam/Forces.js
@@ -1,27 +1,47 @@
-import { DivStyled, LabelStyledName, LabelStyledDimension } from './newBeamStyled'
+import { DivStyled, LabelStyledName, LabelStyledDimension, SectionDivStyled, H2Styled } from './newBeamStyled'
 
 const Forces = (props) => {
 
     return (
-        <>
+        <SectionDivStyled>
+            <H2Styled>Forces</H2Styled>
+
             <DivStyled>
                 <LabelStyledName htmlFor="bendingMoment">Bending Moment</LabelStyledName>
-                <input id="bendingMoment" name='bendingMoment' type="number" placeholder="10" />
+                <input
+                    id="bendingMoment"
+                    name='bendingMoment'
+                    type="number"
+                    placeholder="10"
+                    defaultValue={props.bendingMoment ? props.bendingMoment : ''}
+                />
                 <LabelStyledDimension htmlFor="bendingMoment">[kN.m]</LabelStyledDimension>
             </DivStyled>
 
             <DivStyled>
                 <LabelStyledName htmlFor="shearForce">Shear Force</LabelStyledName>
-                <input id="shearForce" name='shearForce' type="number" placeholder="10" />
+                <input
+                    id="shearForce"
+                    name='shearForce'
+                    type="number"
+                    placeholder="10"
+                    defaultValue={props.shearForce ? props.shearForce : ''}
+                />
                 <LabelStyledDimension htmlFor="shearForce">[kN]</LabelStyledDimension>
             </DivStyled>
 
             <DivStyled>
                 <LabelStyledName htmlFor="torsion">Torsion</LabelStyledName>
-                <input id="torsion" name='torsion' type="number" placeholder="10" />
+                <input
+                    id="torsion"
+                    name='torsion'
+                    type="number"
+                    placeholder="10"
+                    defaultValue={props.torsion ? props.torsion : ''}
+                />
                 <LabelStyledDimension htmlFor="torsion">[kN.m]</LabelStyledDimension>
             </DivStyled>
-        </>
+        </SectionDivStyled>
     )
 }
 
