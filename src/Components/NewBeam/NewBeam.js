f673e2bba71769d2e77232455f2814e7e2711e10
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-09-07 21:19:29 +0300

    UPDATE NewBeam

--- src/Components/NewBeam/NewBeam.js
+++ src/Components/NewBeam/NewBeam.js
@@ -3,15 +3,13 @@ import { useNavigate } from 'react-router-dom';
 
 import * as beamService from '../../services/services';
 import validateNewElements from '../../validations/newDataValidations';
-import * as structuralData from '../../services/structuralData';
-import { MainStyled, FormStyled, ButtonStyled } from './newBeamStyled'
-import { BeamsInfo } from './BeamsInfo'
-import { BeamsGeometry } from './BeamsGeometry'
-import { Concrete } from './Concrete'
-import { Steel } from './Steel'
-import { Forces } from './Forces'
-import { Rebars } from './Rebars'
-import { fcdCalculate, fcmCalculate, fctmCalculate, fydCalculate } from '../../services/formulas';
+import { MainStyled, FormStyled, ButtonStyled, SectionDivStyledTop, SectionDivStyledBottom } from './newBeamStyled';
+import { BeamsInfo } from './BeamsInfo';
+import { BeamsGeometry } from './BeamsGeometry';
+import { Concrete } from './Concrete';
+import { Steel } from './Steel';
+import { Forces } from './Forces';
+import { Rebars } from './Rebars';
 
 
 const NewBeam = () => {
@@ -21,23 +19,14 @@ const NewBeam = () => {
     let [steelError, setSteelError] = useState('');
     let [rebarError, setRebarError] = useState('');
 
-    let [fck, setFck] = useState(0);
-    let [fy, setFy] = useState(0);
-    let [alphaCC, setAlphaCC] = useState(0);
-    let [gammaMC, setGammaMC] = useState(0);
-    let [gammaMS, setGammaMS] = useState(0);
-    let fcd = 0;
-    let fcm = 0;
-    let fctm = 0;
-    let fyd = 0;
-
     const navigate = useNavigate();
 
     const onNewBeamSubmitHandler = (e) => {
         e.preventDefault();
 
-        const { level, height, width, bendingMoment, concrete, steel, rebar, beamsNumber } = e.target;
+        const { level, beamsNumber, height, width, bendingMoment, shearForce, torsion, concrete, steel, rebar, numberRebars } = e.target;
 
+        // ERRORS
         const heightTextError = validateNewElements.height(height.value);
         const widthTextError = validateNewElements.width(width.value);
         const concreteTextError = validateNewElements.concrete(concrete.value);
@@ -81,6 +70,7 @@ const NewBeam = () => {
 
         if (heightTextError || widthTextError || concreteTextError || steelTextError || rebarTextError) return;
 
+        // NEW BEAM ADDED
         beamService
             .addNewBeam(
                 level.value,
@@ -88,9 +78,12 @@ const NewBeam = () => {
                 Number(height.value),
                 Number(width.value),
                 Number(bendingMoment.value),
+                Number(shearForce.value),
+                Number(torsion.value),
                 concrete.value,
                 steel.value,
                 Number(rebar.value),
+                Number(numberRebars.value),
             )
             .then(responce => {
                 if (responce.statusText === 'OK') {
@@ -100,15 +93,12 @@ const NewBeam = () => {
             .catch(console.log);
     }
 
-    fcd = fcdCalculate(fck, alphaCC, gammaMC);
-    fcm = fcmCalculate(fck);
-    fctm = fctmCalculate(fck, fcm);
-    fyd = fydCalculate(fy, gammaMS);
-
     return (
         <MainStyled>
             <FormStyled onSubmit={onNewBeamSubmitHandler}>
-                <h1>New Beam</h1>
+                <SectionDivStyledTop>
+                    <h1>New Beam</h1>
+                </SectionDivStyledTop>
 
                 <BeamsInfo />
 
@@ -120,32 +110,20 @@ const NewBeam = () => {
                 <Forces />
 
                 <Concrete
-                    structuralData={structuralData}
-                    setFck={setFck}
                     concreteError={concreteError}
-                    fck={fck}
-                    setAlphaCC={setAlphaCC}
-                    setGammaMC={setGammaMC}
-                    fcd={fcd}
-                    fcm={fcm}
-                    fctm={fctm}
                 />
 
                 <Steel
-                    structuralData={structuralData}
-                    setFy={setFy}
                     steelError={steelError}
-                    setGammaMS={setGammaMS}
-                    fyd={fyd}
-                    fy={fy}
                 />
 
                 <Rebars
-                    structuralData={structuralData}
                     rebarError={rebarError}
                 />
 
-                <ButtonStyled type="Submit">Save</ButtonStyled>
+                <SectionDivStyledBottom>
+                    <ButtonStyled type="Submit">Save</ButtonStyled>
+                </SectionDivStyledBottom >
 
             </FormStyled>
         </MainStyled >
