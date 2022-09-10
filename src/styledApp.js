2c2b620eb1b28b658163dc85c84df4cd8e0b72bd
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-09-10 20:13:23 +0300

    CREATE styledApp

---
+++ src/styledApp.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const WrapperDivStyled = styled.div`
+position:absolute;
+top:0px;
+left:0px;
+width:100%;
+/* min-height:90vh; */
+background-color: gray;
+text-align: center;
+display:flex;
+flex-direction: column;    
+  
+`;
+
+const MainStyled = styled.main`
+position:relative;
+padding-top:80px;
+width:100%;
+min-height:calc(100vh - 60px - 100px - 40px);
+`;
+
+export {
+  WrapperDivStyled,
+  MainStyled
+};
