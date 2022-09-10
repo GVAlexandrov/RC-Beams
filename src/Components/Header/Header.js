d11a485109c1d8c1cad1d1bffddba2e6b20cd1ad
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-09-10 12:40:25 +0300

    UPDATE Header
    Added drop-down menu for Media

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,14 @@
-import { NavLink, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { logout } from '../../services/authService';
-import styled from 'styled-components';
+import {
+    HeaderStyled,
+    NavStyled,
+    UlStyled,
+    LiStyled,
+    NavLinkStyled,
+    DivHamburgerMenu,
+    SpanHamburgerMenu
+} from './headerStyledComponents';
 
 const Header = ({
     userEmail,
@@ -25,12 +33,19 @@ const Header = ({
         navigate('/');
     };
 
+    const togleMenuHandler = (e) => {
+        const menuStyle = document.getElementById('MenuUL').style;
+
+        menuStyle.display === 'none'
+            ? menuStyle.display = ''
+            : menuStyle.display = 'none';
+    }
 
     return (
         <HeaderStyled>
             <NavStyled >
                 {/* <img src="https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/256x256/plain/bolt.png" alt="" /> */}
-                <UlStyled>
+                <UlStyled id='MenuUL'>
                     {(userEmail !== null)
                         ? <NavLinkStyled to='/beams'><LiStyled>Hello, {localStorage.getItem('email')}</LiStyled></NavLinkStyled>
                         : ''
@@ -57,80 +72,17 @@ const Header = ({
                         : ''
                     }
 
-                    {/* <LiStyled><NavLinkStyled onClick={logoutHandler} to="/">Logout</NavLinkStyled></LiStyled> */}
                 </UlStyled>
-                <button hidden>MENU</button>
+
+                <DivHamburgerMenu onClick={togleMenuHandler}>
+                    <SpanHamburgerMenu></SpanHamburgerMenu>
+                    <SpanHamburgerMenu></SpanHamburgerMenu>
+                    <SpanHamburgerMenu></SpanHamburgerMenu>
+                </DivHamburgerMenu>
+
             </NavStyled >
         </HeaderStyled>
     )
 }
-
-const HeaderStyled = styled.header`
-position: fixed;
-width: 100%;
-top:0px;
-z-index:10;
-`;
-
-const NavStyled = styled.nav`
-background-color: black;
-width:100%;
-height:60px;
-margin:0px;
-border:1px;
-border-bottom-left-radius:80px;
-
-display:flex;
-justify-content:end;
-align-items:center;
-font-size: 23px;
-top:0px;
-left:0px;
-min-width:690px;
-/* z-index:1; */
-
-&:hover{
-    color:red;    
-}
-`
-
-const UlStyled = styled.ul`
-position:relative;
-list-style:none;
-text-align:right;
-right:15px;
-display:flex;
-
-@media only screen and (max-width: 690px) {
-    position:absolute;
-    top:0px;
-    right:0px;
-    margin:0px;
-    padding:0px;
-    padding-top:60px;
-    flex-direction: column;    
-    background-color:black;
-    width:100vw;
-    z-index:100;
-    text-align:center;
-    min-width:260px;
-    /* display:none; */
-  }
-`;
-
-const LiStyled = styled.li`
-display:inline-block;
-margin:15px;
-`;
-
-const NavLinkStyled = styled(NavLink)`
-  text-decoration:none;
-  color: white;
-  
-  &:hover{
-    color:red;
-}
-`;
-
 
 export default Header;
