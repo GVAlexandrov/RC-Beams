ec5cd75cc4a1cf046e8efa255e50072b9be511fe
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-09-12 22:25:19 +0300

    UPDATE App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 // import logo from './logo.svg';
 import { Routes, Route } from 'react-router-dom';
-import styled from 'styled-components';
 import { auth } from './config/firebaseInit';
 import { useEffect, useState } from 'react';
-
+import { WrapperDivStyled, MainStyled } from './styledApp';
 
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
@@ -12,6 +11,7 @@ import Register from './Components/Register/Register';
 import Beams from './Components/Beams/Beams';
 import NewBeam from './Components/NewBeam/NewBeam';
 import EditBeam from './Components/EditBeam/EditBeam';
+import InfoBeam from './Components/InfoBeam/InfoBeam';
 import Frames from './Components/Frames/Frames';
 import Footer from './Components/Footer/Footer';
 
@@ -28,6 +28,9 @@ function App() {
       <Header userEmail={userEmail} setUserEmail={setUserEmail} />
 
       <MainStyled>
+        <h1>This webpage is still under construction</h1>
+        <h3>Some of the features might not work</h3>
+
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
@@ -36,6 +39,7 @@ function App() {
           <Route path='/beams' element={<Beams />} />
           <Route path='/beams/new-beam' element={<NewBeam />} />
           <Route path='/beams/edit-beam/:beamId' element={<EditBeam />} />
+          <Route path='/beams/info-beam/:beamId' element={<InfoBeam />} />
 
           <Route path='/frames' element={<Frames />} />
         </Routes>
@@ -46,25 +50,5 @@ function App() {
     </WrapperDivStyled>
   );
 }
-
-const WrapperDivStyled = styled.div`
-position:absolute;
-top:0px;
-left:0px;
-width:100%;
-/* min-height:90vh; */
-background-color: gray;
-text-align: center;
-display:flex;
-flex-direction: column;    
-  
-`;
-
-const MainStyled = styled.main`
-position:relative;
-padding-top:80px;
-width:100%;
-min-height:calc(100vh - 60px - 100px - 40px);
-`;
 
 export default App;
