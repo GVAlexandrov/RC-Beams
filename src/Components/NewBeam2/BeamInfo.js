14c8301378d26d3eff697c5a7f0354b9b95dbbfa
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-09-18 13:08:05 +0300

    CREATE BeamInfo

---
+++ src/Components/NewBeam2/BeamInfo.js
@@ -0,0 +1,88 @@
+import {
+    TableStyled,
+    InputStyled,
+    TdStyled,
+    TdStyledDimensions,
+} from './newBeam2Styled';
+
+const BeamInfo = (props) => {
+    let projectName = props.projectName;
+    let setProjectName = props.setProjectName;
+    let beamLevel = props.beamLevel;
+    let setBeamLevel = props.setBeamLevel;
+    let beamNumberString = props.beamNumberString;
+    let setbeamNumberString = props.setbeamNumberString;
+    let setRebarPosition = props.setRebarPosition;
+
+    return (
+        <TableStyled>
+            <thead>
+                <tr>
+                    <TdStyled>Project name</TdStyled>
+                    <TdStyled>Level</TdStyled>
+                    <TdStyled>Beam</TdStyled>
+                    <TdStyled>Reinforcement</TdStyled>
+                </tr>
+
+                <tr>
+                    <TdStyledDimensions>[-]</TdStyledDimensions>
+                    <TdStyledDimensions>[-]</TdStyledDimensions>
+                    <TdStyledDimensions>[№]</TdStyledDimensions>
+                    <TdStyledDimensions>[position]</TdStyledDimensions>
+                </tr>
+            </thead>
+
+
+            <tbody>
+                <tr>
+                    <TdStyled>
+                        <InputStyled
+                            id="projectsName"
+                            name='projectsName'
+                            type="text"
+                            placeholder="Porject's name"
+                            style={{ 'min-width': '150px' }}
+                            value={projectName}
+                            onChange={e => setProjectName(e.target.value)}
+                        />
+                    </TdStyled>
+
+                    <TdStyled>
+                        <InputStyled
+                            id="level"
+                            name='level'
+                            type="number"
+                            step='0.01'
+                            value={beamLevel}
+                            onChange={e => setBeamLevel(Number(e.target.value))}
+                        />
+                    </TdStyled>
+
+                    <TdStyled>
+                        <InputStyled
+                            id="projectsName"
+                            name='projectsName'
+                            type="text"
+                            placeholder="B01"
+                            value={beamNumberString}
+                            onChange={e => setbeamNumberString(e.target.value)}
+                        />
+                    </TdStyled>
+
+                    <TdStyled>
+                        <select name="steel" id="steel" onChange={e => setRebarPosition(e.target.value)}>
+
+                            <option disabled selected hidden value="default">-</option>
+
+                            < option value='top' >Top</option>
+                            < option value='bottom' >Bottom</option>
+                        </select>
+                    </TdStyled>
+
+                </tr>
+            </tbody>
+        </TableStyled>
+    )
+}
+
+export default BeamInfo;
