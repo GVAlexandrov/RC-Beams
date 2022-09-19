51cf6e0236bfcbd3f1ed447db162e736f9d6346f
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-09-20 00:24:16 +0300

    UPDATE Home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,20 +5,27 @@ const Home = () => {
     const email = localStorage.email;
 
     useEffect(() => {
-        if (email) {
-            setIsLogged(true)
-        } else {
-            setIsLogged(false)
-        }
+        email
+            ? setIsLogged(true)
+            : setIsLogged(false)
     }, [email])
 
     return (
-        <h1>
-            Hello, {
-                isLogged
-                    ? email
-                    : 'Guest.'}
-        </h1>
+        <>
+            <h1>
+                Hello, {email || 'Guest.'}
+            </h1>
+
+            {isLogged
+                ? (<>
+                    <p>If you want to design new reinforced concrete (RC) beam, please, visit the New Beam menu from the navigation bar above.</p>
+                    <p>If you want to check or edit your saved designs, please, visit the RC Beams menu</p>
+                </>)
+                : (<>
+                    <p>This application allows you to design reinforced concrete (RC) beams, subjected to bending moment. If you want to use it, please, Login. If you don't have an account, you should use the Register menu.</p>
+                </>)
+            }
+        </>
     );
 
 }
