9488ef0c156bd507db8e71f899c9de3aab9880e1
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-09-19 21:22:30 +0300

    UPDATE App

--- src/App.js
+++ src/App.js
@@ -7,14 +7,10 @@ import { WrapperDivStyled, MainStyled } from './styledApp';
 
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
-// import Login from './Components/Login/Login';
-// import Register from './Components/Register/Register';
 import LoginRegister from './Components/LoginRegister/LoginRegister';
 import Beams from './Components/Beams/Beams';
-import NewBeam from './Components/NewBeam/NewBeam';
 import NewBeam2 from './Components/NewBeam2/NewBeam2';
-import EditBeam from './Components/EditBeam/EditBeam';
-import InfoBeam from './Components/InfoBeam/InfoBeam';
+import EditBeam2 from './Components/EditBeam2/EditBeam2';
 import Footer from './Components/Footer/Footer';
 
 
@@ -30,8 +26,8 @@ function App() {
       <Header userEmail={userEmail} setUserEmail={setUserEmail} />
 
       <MainStyled>
-        <h1>This webpage is still under construction</h1>
-        <h3>Some of the features might not work</h3>
+        <h1 style={{ 'color': 'red' }}>This webpage is still under construction</h1>
+        <h3 style={{ 'color': 'red' }}>Some of the features might not work</h3>
 
         <Routes>
           <Route path='/' element={<Home />} />
@@ -39,10 +35,8 @@ function App() {
           <Route path='/register' element={<LoginRegister />} />
 
           <Route path='/beams' element={<Beams />} />
-          <Route path='/beams/new-beam' element={<NewBeam />} />
-          <Route path='/beams/new-beam2' element={<NewBeam2 />} />
-          <Route path='/beams/info-beam/:beamId' element={<InfoBeam />} />
-          <Route path='/beams/edit-beam/:beamId' element={<EditBeam />} />
+          <Route path='/beams/new-beam' element={<NewBeam2 />} />
+          <Route path='/beams/edit-beam/:beamId' element={<EditBeam2 />} />
         </Routes>
       </MainStyled>
 
