29321f478a5181982b8f93f0a58f20c4320d620f
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-11-28 21:33:15 +0200

    UPDATE WallCanvas

--- src/Components/NewWall/WallCanvas.js
+++ src/Components/NewWall/WallCanvas.js
@@ -6,28 +6,39 @@ import styled from 'styled-components';
 const WallCanvas = (props) => {
     const canvasRef = useRef(null);
 
+    let width = props.width;
+    console.log(width);
+
     useEffect(() => {
         const canvas = canvasRef.current;
         if (!canvas) return;
+        canvas.width = 500;
+        canvas.height = 500;
+        console.log(canvas);
 
         const context = canvas.getContext('2d');
         if (!context) return;
 
         context.beginPath();
-        context.moveTo(100, 200);
-        context.lineTo(200, 250);
-        context.lineTo(200, 400);
-        context.lineTo(50, 400);
+        context.moveTo(0, canvas.height / 2);
+        context.lineTo(canvas.width, canvas.height / 2);
+
+        context.moveTo(canvas.width / 2, 0);
+        context.lineTo(canvas.width / 2, canvas.height);
+
+        context.moveTo(0, 0);
+        context.lineTo(width, width);
+        context.lineTo(-width, 8);
         context.stroke();
-    }, [])
+
+    }, [width])
 
     return (
-        <>
+        <DivCanvasWrapperStyled>
             <h1>Graph</h1>
-            <CanvasStyled ref={canvasRef}>
 
-            </CanvasStyled >
-        </>
+            <CanvasStyled ref={canvasRef} />
+        </DivCanvasWrapperStyled>
     )
 }
 
@@ -37,6 +48,10 @@ border:1px solid;
 width:500px;
 height:500px;
 margin: 50px 0px;
+`;
+
+const DivCanvasWrapperStyled = styled.div`
+margin: 20px;
 `;
 
 
