57d38ed9c61a8b311c8a7df01628101f97c3cfee
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-12-13 21:17:23 +0200

    CREATE NewWallStyled

---
+++ src/Components/NewWall/newWallStyled.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+
+const ButtonStyled = styled.button`
+display: inline-block;
+margin:20px 5px;
+margin-bottom:20px;
+font-size:16px;
+background:#bdbbb7;
+padding: 10px 50px;
+border-radius:5px;
+border-color:black;
+cursor: pointer;
+&:hover{
+background-color:#969592;
+}
+&:active {
+background-color:#bdbbb7;
+/* background-color:black;
+color:red;
+border-color:red; */
+}
+`;
+
+
+const DivErrorStyled = styled.span`
+color:red;
+background-color:white;
+font-size:20px;
+/* font-weight: bold; */
+border: 1.5px solid red;
+border-top-right-radius:15px;
+border-bottom-left-radius:15px;
+display: inline-block;
+`;
+
+
+const PErrorStyled = styled.p`
+margin:auto;
+padding:10px;
+`;
+
+
+export {
+    ButtonStyled,
+    DivErrorStyled,
+    PErrorStyled
+}
