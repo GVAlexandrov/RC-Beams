a19901d53de3b8b9ccc3ef99396013844affcdfe
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2023-09-19 03:06:53 +0300

    UPDATE styledApp.js

--- src/styledApp.js
+++ src/styledApp.js
@@ -1,24 +1,23 @@
 import styled from 'styled-components';
 
 const WrapperDivStyled = styled.div`
-position:absolute;
-top:0px;
-left:0px;
-width:100%;
-/* min-height:90vh; */
-background-color: #bdbbb7;
-background-image:radial-gradient(whitesmoke,hsl(0, 0%, 20%));
-text-align: center;
-display:flex;
-flex-direction: column;    
-  
+  position:absolute;
+  top:0px;
+  left:0px;
+  width:100%;
+  /* min-height:90vh; */
+  /* background-color: white; */
+  background-image:radial-gradient(whitesmoke,hsl(0, 0%, 20%));
+  text-align: center;
+  display:flex;
+  flex-direction: column;
 `;
 
 const MainStyled = styled.main`
-position:relative;
-padding-top:80px;
-width:100%;
-min-height:calc(100vh - 60px - 100px - 40px);
+  position:relative;
+  padding-top:80px;
+  width:100%;
+  min-height:calc(100vh - 60px - 100px - 40px);
 `;
 
 export {
